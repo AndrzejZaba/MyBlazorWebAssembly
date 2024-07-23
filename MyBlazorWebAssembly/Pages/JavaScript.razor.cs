5832d513857088b430e736efbabd9ab89ea500d5
AndrzejZaba <andrzejzaba@onet.pl>
2024-07-23 20:12:53 +0200

    js alert

---
+++ MyBlazorWebAssembly/Pages/JavaScript.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace MyBlazorWebAssembly.Pages;
+
+public partial class Javascript
+{
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+    private async Task DisplayAlert()
+    {
+        await JSRuntime.InvokeVoidAsync("alert", "Przykładowa wiadomość");
+    }
+}
